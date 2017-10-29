849e3627eca8656a46ee240d78f59df8e20b2bc3
Avetik Harutyunyan <avetik.harutyunyan1@gmail.com>
2017-10-29 11:52:55 +0300

    Implement customer and order creating

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const Router = require('koa-router');
 const logger = require('koa-morgan');
 const bodyParser = require('koa-body');
 const { Order, Customer } = require('./db');
+const v4 = require('uuid/v4');
 
 const server = new Koa();
 const router = new Router();
@@ -20,6 +21,20 @@ router.get('/order', async (ctx) => {
   };
 });
 
+router.post('/order', async (ctx) => {
+  const order = {
+    id: v4(),
+    weight: ctx.request.body.weight,
+    price_per_kilo: ctx.request.body.price_per_kilo,
+    sold_at: ctx.request.body.sold_at,
+    customer_id: ctx.request.body.customer_id,
+  };
+  await Order.create(order);
+  ctx.body = {
+    order,
+  };
+});
+
 router.get('/customer', async (ctx) => {
   const customers = await Customer.findAll({
     include: [Order],
@@ -33,6 +48,17 @@ router.get('/customer/:id', async (ctx) => {
   const customer = await Customer.findById(ctx.params.id, {
     include: [Order],
   });
+  ctx.body = {
+    customer,
+  };
+});
+
+router.post('/customer', async (ctx) => {
+  const customer = {
+    id: v4(),
+    name: ctx.request.body.name,
+  };
+  await Customer.create(customer);
   ctx.body = {
     customer,
   };
